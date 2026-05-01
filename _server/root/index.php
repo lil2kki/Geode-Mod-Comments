ddfc806bfe3ee0f0434c29856530c27f53329362
lil2kki <latterrarity70@gmail.com>
2026-05-01 16:10:04 +0300

    Add CORS headers to index.php

--- _server/root/index.php
+++ _server/root/index.php
@@ -1,5 +1,15 @@
 <?php
 
+header('Access-Control-Allow-Origin: https://geode-sdk.org');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Max-Age: 86400');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 define('MAX_BODY_LENGTH', 500);
 
 $ARGON_URL = "https://argon.globed.dev/v1";
